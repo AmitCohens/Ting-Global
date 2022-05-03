e3550cad8dfca57b7206148294843bdf01ede41e
Amit Cohen <cohen.amit.official@gmail.com>
2022-05-03 12:16:26 +0300

    Home page 1.0

--- Screens/HomePage.js
+++ Screens/HomePage.js
@@ -1,2 +1,11 @@
 import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+const homePage = () => {
+  <View>
+    <View ID={"firstLine"}>
+
+    </View>
+
+  </View>
+};
+export default homePage;
