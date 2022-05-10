ad0cd0331e8728baf6793cc728ed39a3e036c395
Amit Cohen <cohen.amit.official@gmail.com>
2022-05-10 18:01:12 +0300

    Gad commit 5.10.22

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -1,11 +1,24 @@
-import {Image, StyleSheet, Text, View} from 'react-native';
+import { Image, StatusBar, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+
 const Page = () => {
+
     return (
         <View name={"body"} style={styleProfilePage.body}>
-            <View name={"details"} style={styleProfilePage.details}>
-                <Image source={require('../Images/Sharon4.png')} style={styleProfilePage.image}/>
+            <View name={"details"} style={styleProfilePage.AllDetails}>
+                <Image source={require('../Images/left-arrow.png')} style={styleProfilePage.arrow} />
+                <View style={styleProfilePage.details}>
                 <Text style={styleProfilePage.text}>{"Sharon Gal-Or"}</Text>
+                <Image source={require('../Images/Sharon4.png')} style={styleProfilePage.image}/>
+                <Text style={styleProfilePage.score}>{"SCORE: 4300"}</Text>
+                </View>
+            </View>
+            <View style={styleProfilePage.container}>
+                <Text style={styleProfilePage.item}>{"EXPRESSION"}</Text>
+                <Text style={styleProfilePage.item}>{"DILIGENCE"}</Text>
+                <Text style={styleProfilePage.item}>{"UNDERSTANDING"}</Text>
+                <Text style={styleProfilePage.item}>{"COURAGE"}</Text>
+                <Text style={styleProfilePage.item}>{"KNOWLEDGE"}</Text>
             </View>
         </View>
     );
@@ -14,33 +27,64 @@ const styleProfilePage = StyleSheet.create({
     text:{
         textAlign: 'center',
         fontSize: 30,
-        justifyContent:'space-around',
         borderTopWidth:4,
         borderBottomWidth:4,
         borderStyle:'solid',
         borderColor: "black",
         fontWeight: 'bold',
         fontFamily: 'sans-serif-medium',
+        margin:10,
+    },
+    score:{
+        textAlign: 'center',
+        fontSize: 25,
+        borderBottomWidth:4,
+        borderStyle:'solid',
+        borderColor: "black",
+        fontWeight: 'bold',
+        fontFamily: 'sans-serif-medium',
+        margin:10,
+    },
+    arrow:{
+        width: 30,
+        height: 30,
     },
     body:{
         backgroundColor:'rgb(255,255,255)',
         height:'100%',
         width:'100%',
+        justifyContent:'space-between',
     },
     image:{
         width: 180,
         height: 180,
         borderRadius: 200,
-        borderStyle:'solid',
         borderWidth: 5,
         borderColor: "blue",
         margin:10,
     },
-    details:{
+    AllDetails:{
         justifyContent:'space-between',
-        alignItems: 'center',
-        paddingTop:30,
+        paddingTop:23,
         backgroundColor:'rgba(1,162,255,0.53)',
     },
+    details:{
+        alignItems: 'center',
+    },
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+        backgroundColor: '#324bb2',
+        padding: 10,
+        margin:2,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        fontSize: 16,
+        borderRadius:25,
+    },
+
 });
 export default Page;
+
