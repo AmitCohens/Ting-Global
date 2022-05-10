37654a336247a92f41a68e890a9384ac39362d6f
Amit Cohen <cohen.amit.official@gmail.com>
2022-05-10 14:58:14 +0300

    Amit commit 5.10.22

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -1,18 +1,39 @@
-import {
-    Image,
-    StyleSheet,
-    Text,
-    View,
-} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 const Page = () => {
     return (
-        <View>
-            <Text>{"This is the Profile page"}</Text>
+        <View name={"body"} style={styleProfilePage.body}>
+            <View name={"details"} style={styleProfilePage.details}>
+                <Image source={require('../Images/Sharon2.png')} style={styleProfilePage.image}/>
+            </View>
+            <Text style={styleProfilePage.text}>{"This is the Profile page"}</Text>
         </View>
     );
 };
 const styleProfilePage = StyleSheet.create({
-
-})
+    text:{
+        textAlign: 'center',
+        fontSize: 30,
+        justifyContent:'space-around',
+    },
+    body:{
+        backgroundColor:'rgb(255,255,255)',
+        height:'100%',
+        width:'100%',
+    },
+    image:{
+        width: 200,
+        height: 200,
+        borderRadius: 200,
+        borderStyle:'solid',
+        borderWidth: 3,
+        borderColor: "black",
+    },
+    details:{
+        justifyContent:'center',
+        paddingTop:30,
+        paddingLeft:'26%',
+        backgroundColor:'rgba(13,195,227,0.53)',
+    },
+});
 export default Page;
