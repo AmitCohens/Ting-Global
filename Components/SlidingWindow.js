2166731a13fa679d1982b3082f97b64ae3bea5ea
Mashriki <imashriki@gmail.com>
2022-05-27 19:51:00 +0300

    Idan - Shabat Shalom

--- Components/SlidingWindow.js
+++ Components/SlidingWindow.js
@@ -10,46 +10,75 @@ import View_Slider from 'react-native-view-slider'
 
 const { width, height } = Dimensions.get('window');
 
+
+
 function App() {
+
+    const data = [
+        {
+            text: "Is Value-Based Hybrid Learning the Solution to Current Global Crisis?",
+        },
+        {
+            text: "Challenge for change in Kenya",
+        },
+        {
+            text: "Solving Global Challenges",
+        },
+        {
+            text: "BREAKING: Challenge 18",
+        },
+        {
+            text: "The 17-Goals trails & Future of Humanity",
+        }
+    ]
+
     return (
-        <>
+        <View style={styles.body}>
+            <Text style={styles.title}>- Latest News -</Text>
             <View_Slider
                 renderSlides = {
                     <>
                         <View style={styles.viewBox}>
-                            <Image source={{uri: 'https://image.shutterstock.com/image-photo/beautiful-autumn-scene-hintersee-lake-260nw-747646759.jpg'}}
-                                   style={{height: 200, width}}/>
+                            <Image source={require("../Images/slide0.jpeg")} style={styles.img}/>
+                            <Text style={styles.text}>{data[0].text}</Text>
                         </View>
                         <View style={styles.viewBox}>
-                            <Image source={{uri: 'https://image.shutterstock.com/z/stock-photo-closeup-black-coffee-cup-top-view-set-1937506825.jpg'}}
-                                   style={{height: 200, width}}/>
+                            <Image source={require("../Images/slide1.jpeg")} style={styles.img}/>
+                            <Text style={styles.text}>{data[1].text}</Text>
                         </View>
                         <View style={styles.viewBox}>
-                            <Image source={{uri: 'https://image.shutterstock.com/image-photo/beach-waves-top-view-turquoise-600w-1239753184.jpg'}}
-                                   style={{height: 200, width}}/>
+                            <Image source={require("../Images/slide2.png")} style={styles.img}/>
+                            <Text style={styles.text}>{data[2].text}</Text>
                         </View>
                         <View style={styles.viewBox}>
-                            <Image source={{uri: 'https://image.shutterstock.com/image-photo/scenic-beauty-landscape-scene-view-600w-2051112362.jpg'}}
-                                   style={{height: 200, width}}/>
+                            <Image source={require("../Images/slide3.jpeg")} style={styles.img}/>
+                            <Text style={styles.text}>{data[3].text}</Text>
+                        </View>
+                        <View style={styles.viewBox}>
+                            <Image source={require("../Images/slide4.jpeg")} style={styles.img}/>
+                            <Text style={styles.text}>{data[4].text}</Text>
                         </View>
                     </>
                 }
 
                 style={styles.slider}     //Main slider container style
-                height = {200}    //Height of your slider
-                slideCount = {4}    //How many views you are adding to slide
+                height = {250}    //Height of your slider
+                slideCount = {5}    //How many views you are adding to slide
                 dots = {false}     // Pagination dots visibility true for visibile
                 dotActiveColor = 'red'     //Pagination dot active color
                 dotInactiveColor = 'gray'    // Pagination do inactive color
                 dotsContainerStyle={styles.dotContainer}     // Container style of the pagination dots
-                autoSlide = {false}    //The views will slide automatically
-                slideInterval = {4000}    //In Miliseconds
+                autoSlide = {true}    //The views will slide automatically
+                slideInterval = {10000}    //In Miliseconds
             />
-        </>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
+    body: {
+        alignItems: 'center'
+    },
     viewBox: {
         paddingHorizontal: 20,
         justifyContent: 'center',
@@ -66,6 +95,15 @@ const styles = StyleSheet.create({
     },
     dotContainer: {
         backgroundColor: 'transparent',
+    },
+    img: {
+        height: 200, width
+    },
+    text: {
+        fontSize: 20
+    },
+    title: {
+        fontSize: 25
     }
 });
 
--- Components/Tabs.js
+++ Components/Tabs.js
@@ -8,78 +8,79 @@ import AllChats from "../Screens/AllChats";
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
-  return (
-    <Tab.Navigator
-        initialRouteName="Home"
-      screenOptions={{
-        headerShown: false,
-        tabBarHideOnKeyboard: true,
+    return (
+        <Tab.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+                headerShown: false,
+                tabBarHideOnKeyboard: true,
 
-        tabBarStyle: {
-        height: 60,
-        backgroundColor: '#ffffff',
-    }}}>
-      <Tab.Screen
-        name="Chats"
-        component={AllChats}
-        options={{
-          tabBarIcon: () => (
-            <Image
-              source={require('../Images/speech-bubble.png')}
-              style={{width: 35, height: 35}}
+                tabBarStyle: {
+                    height: 60,
+                    backgroundColor: '#ffffff',
+                }
+            }}>
+            <Tab.Screen
+                name="Chats"
+                component={AllChats}
+                options={{
+                    tabBarIcon: () => (
+                        <Image
+                            source={require('../Images/speech-bubble.png')}
+                            style={{width: 35, height: 35}}
+                        />
+                    ),
+                }}
             />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Actions"
-        component={SignUp}
-        options={{
-          tabBarIcon: () => (
-            <Image
-              source={require('../Images/call.png')}
-              style={{width: 35, height: 35}}
+            <Tab.Screen
+                name="Actions"
+                component={SignUp}
+                options={{
+                    tabBarIcon: () => (
+                        <Image
+                            source={require('../Images/call.png')}
+                            style={{width: 35, height: 35}}
+                        />
+                    ),
+                }}
             />
-          ),
-        }}
-      />
-        <Tab.Screen
-            name="Home"
-            component={HomePage}
-            options={{
-                tabBarIcon: () => (
-                    <Image
-                        source={require('../Images/home1.png')}
-                        style={{width: 35, height: 35}}
-                    />
-                ),
-            }}
-        />
-      <Tab.Screen
-        name="International"
-        component={SignUp}
-        options={{
-          tabBarIcon: () => (
-            <Image
-              source={require('../Images/number-18.png')}
-              style={{width: 35, height: 35}}
+            <Tab.Screen
+                name="Home"
+                component={HomePage}
+                options={{
+                    tabBarIcon: () => (
+                        <Image
+                            source={require('../Images/home1.png')}
+                            style={{width: 35, height: 35}}
+                        />
+                    ),
+                }}
             />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="accomplishments"
-        component={SignUp}
-        options={{
-          tabBarIcon: () => (
-            <Image
-              source={require('../Images/love.png')}
-              style={{width: 35, height: 35}}
+            <Tab.Screen
+                name="International"
+                component={SignUp}
+                options={{
+                    tabBarIcon: () => (
+                        <Image
+                            source={require('../Images/number-18.png')}
+                            style={{width: 35, height: 35}}
+                        />
+                    ),
+                }}
             />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
+            <Tab.Screen
+                name="Accomplishments"
+                component={SignUp}
+                options={{
+                    tabBarIcon: () => (
+                        <Image
+                            source={require('../Images/love.png')}
+                            style={{width: 35, height: 35}}
+                        />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
+    );
 };
 export default Tabs;
