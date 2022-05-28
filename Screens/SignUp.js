35250b0f06f9c1a378b8b539df36265948743155
Amit Cohen <cohen.amit.official@gmail.com>
2022-05-29 01:53:34 +0300

    Amit 29.5.22 1:52AM

--- Screens/SignUp.js
+++ Screens/SignUp.js
@@ -1,16 +1,23 @@
-import React from 'react';
 import {Text, View, ScrollView, StyleSheet} from 'react-native';
 import {TextInput} from 'react-native-paper';
+import * as React from "react";
 import AppButton from '../Components/AppButton';
+import CountryPicker from 'react-native-country-picker-modal'
+import { CountryDropdown, RegionDropdown } from 'react-country-region-selector';
 
 const Signup = ({navigation}) => {
   return (
     <View style={styles.view}>
         <Text style={styles.text}>Sign up</Text>
-        <TextInput label="Name" style={styles.input}/>
-        <TextInput label="Email" keyboardType="email-address" style={styles.input}/>
+        <TextInput label="Username" style={styles.input}/>
         <TextInput label="Phone number" keyboardType="phone-pad" style={styles.input}/>
-        <AppButton title="Submit" />
+        <TextInput label="Full name" style={styles.input}/>
+        <TextInput label="Country" style={styles.input}/>
+        <TextInput label="Email address" keyboardType="email-address" style={styles.input}/>
+        <TextInput label="Challenge language" style={styles.input}/>
+        <View style={styles.button}>
+            <AppButton title="Submit" type="submit" style={styles.button}/>
+        </View>
     </View>
   );
 };
@@ -30,11 +37,18 @@ const styles = StyleSheet.create({
     },
     input: {
         // marginBottom: 10
-        margin: 10,
+        margin: 5,
 
     },
     text: {
         textAlign: 'center',
         fontSize: 25
+    },
+    button:{
+        paddingRight: "10%",
+        paddingLeft: "10%",
+        paddingTop: "10%",
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 });
