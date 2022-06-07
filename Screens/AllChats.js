be3c1a0ea31675d023a07c8402ca443d11e652fe
Amit Cohen <cohen.amit.official@gmail.com>
2022-06-07 10:38:02 +0300

    Amit 7.6.22 10:37 AM

--- Screens/AllChats.js
+++ Screens/AllChats.js
@@ -34,7 +34,7 @@ const MessagesScreen = ({navigation}) => {
                                     <UserName>{item[1].name}</UserName>
                                     <PostTime>{"Challenge Day " + item[1].day}</PostTime>
                                 </UserInfoText>
-                                <MessageText>{item[1].msg_history[0].text}</MessageText>
+                                <MessageText>{item[1].msg_history[0].user.name}</MessageText>
                             </TextSection>
                         </UserInfo>
                     </Card>
